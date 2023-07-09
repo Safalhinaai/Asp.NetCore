0076042ec041ad2f8fb34ac211b3c5788d95be27
Safalhinaai <safaalhinaai@gmail.com>
2023-07-09 17:02:40 +0400

    First commit

---
+++ WebApplication1/Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TodoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create() { return View(); }
+
+        public IActionResult Update(int id)
+        {
+            var todoobj = _db.Todos.FirstOrDefault(b=> b.Id == id );
+            return View(todoobj);
+        }
+
+        public IActionResult Update(Todo obj)
+        {
+            _db.Todos.Update(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var todoobj =_db.Todos.FirstOrDefault(b=> b.Id
+            == id);
+            _db.Todos.Remove(todoobj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+  
+{
+    public class ApplicationDbContext: DbContext
+    {
+       
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+
+        }
+
+        public  DbSet<Todo> Todos { get; set; }
+      
+    }
+}
